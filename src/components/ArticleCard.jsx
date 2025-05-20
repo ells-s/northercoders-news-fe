5a93411d8d3435198a0dc46708608bc370750f70
ells-s <182009067+ells-s@users.noreply.github.com>
2025-05-20 14:24:53 +0100

    completed basic structure of the articles page, with sorting functionality

---
+++ src/components/ArticleCard.jsx
@@ -0,0 +1,33 @@
+import { Link } from "react-router-dom"
+
+
+function ArticleCard({ articleImg, articleTitle, articleDate, articleAuthor, articleTopic, articleVotes, articleCommentCount, articleId }) {
+
+    return (
+        <article className="article-card">
+            <div className="article-card-img-container">
+                <Link to={`articles/${articleId}`}>
+                    <img src={articleImg} />
+                </Link>
+            </div>
+            <div className="article-card-info-container">
+                <p className="article-card-date">{articleDate}</p>
+                <Link to={`articles/${articleId}`}>
+                    <h3 className="article-card-title">{articleTitle}</h3>
+                </Link>
+                <Link to={`users/${articleAuthor}`}>
+                    <p className="article-card-author">By {articleAuthor}</p>
+                </Link>
+                <Link to={`topics/${articleTopic}`}>
+                    <p className="article-card-topic">{articleTopic}</p>
+                </Link>
+            </div>
+            <div className="article-card-details-container">
+                <p className="article-card-votes">Votes: {articleVotes}</p>
+                <p className="article-card-comments">Comments: {articleCommentCount}</p>
+            </div>
+        </article>
+    )
+}
+
+export default ArticleCard
