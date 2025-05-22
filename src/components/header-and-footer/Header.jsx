4feadd516095974e2ca37cb38c0abda8267d7328
ells-s <182009067+ells-s@users.noreply.github.com>
2025-05-22 08:39:07 +0100

    change to the header

--- src/components/header-and-footer/Header.jsx
+++ src/components/header-and-footer/Header.jsx
@@ -7,21 +7,34 @@ function Header({ username, setUsername }) {
     let navigate = useNavigate();
     //fetch topics loop through to display topics as links
 
+    const [navbarOpen, setNavBarOpen] = useState(false)
+
+    function handleNavbarView(event) {
+        event.preventDefault();
+        setNavBarOpen(!navbarOpen)
+    }
+
     function handleLogOut(event) {
         event.preventDefault();
         setUsername("")
         navigate("/")
     }
 
+
+
     return (
         <header className="header">
-            <nav>
-                <Link to="/">Home</Link>
-                {username ? <Link to={`/users/${username}`}>Profile</Link> : <Link to="/login">Login</Link>}
-                <Link to="/articles">Articles</Link>
-                {username ? <button onClick={handleLogOut}>log out</button> : null}
-
-            </nav>
+            <button onClick={handleNavbarView}>menu</button>
+            {navbarOpen ?
+                <nav>
+                    <Link to="/">Home</Link>
+                    {username ? <Link to={`/users/${username}`}>Profile</Link> : <Link to="/login">Login</Link>}
+                    <Link to="/articles">Articles</Link>
+                    {username ? <button onClick={handleLogOut}>log out</button> : null}
+                </nav>
+                :
+                null
+            }
         </header >
     )
 }
